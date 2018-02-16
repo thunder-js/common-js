c54e39945aa6ec7db79bd3deab62510f2be0c4db
Rafael Ribeiro <rafael.correia.poli@gmail.com>
2018-02-16 01:18:28 -0200

    fix(math): A small fix

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export const sum = (a: number, b: number): number => a + b
 export const subtract = (a: number, b: number): number => a - b
 export const multiply = (a: number, b: number): number => a * b
-export const divide = (a: number, f: number): number => a / f
+export const divide = (a: number, g: number): number => a / g
 export interface ILifecycle {
   start: () => void;
   stop: () => void;
