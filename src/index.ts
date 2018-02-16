d06f7efb4abf5f64060e5763c49570fb588d3bb8
Rafael Ribeiro <rafael.correia.poli@gmail.com>
2018-02-16 01:29:30 -0200

    feat(math): Changed from z do y
    It is very important to call this variable y instead of z

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export const sum = (a: number, b: number): number => a + b
 export const subtract = (a: number, b: number): number => a - b
 export const multiply = (a: number, b: number): number => a * b
-export const divide = (a: number, z: number): number => a / z
+export const divide = (a: number, y: number): number => a / y
 export interface ILifecycle {
   start: () => void;
   stop: () => void;
