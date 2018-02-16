bc90a2064794b923033ddc2a93d3b3a123ed701d
Rafael Ribeiro <rafael.correia.poli@gmail.com>
2018-02-15 22:44:39 -0200

    feat: Added subtract function
    A new function was added, it is a math function called subtract
    
    fix #1

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export const sum = (a: number, b: number): number => a + b
-
+export const subtract = (a: number, b: number): number => a - b
 export interface ILifecycle {
   start: () => void;
   stop: () => void;
