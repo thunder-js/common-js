60ff65558631abc1444b2e203f843df5677e2a00
Rafael Ribeiro <rafael.correia.poli@gmail.com>
2018-02-16 01:22:30 -0200

    feat(math): Changed to Z

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export const sum = (a: number, b: number): number => a + b
 export const subtract = (a: number, b: number): number => a - b
 export const multiply = (a: number, b: number): number => a * b
-export const divide = (a: number, g: number): number => a / g
+export const divide = (a: number, z: number): number => a / z
 export interface ILifecycle {
   start: () => void;
   stop: () => void;
