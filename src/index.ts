9059cfda8f53cd8b86ddeb4ba1d9726575c0a459
Rafael Ribeiro <rafael.correia.poli@gmail.com>
2018-02-16 01:04:10 -0200

    feat(Y):

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export const sum = (a: number, b: number): number => a + b
 export const subtract = (a: number, b: number): number => a - b
 export const multiply = (a: number, b: number): number => a * b
-export const divide = (a: number, d: number): number => a / d
+export const divide = (a: number, e: number): number => a / e
 export interface ILifecycle {
   start: () => void;
   stop: () => void;
